aa989641e3f0449e89527ee60c188ec115e4cc82
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-13 14:19:42 +0300

    Implemented track service to handle API requests

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,9 +1,31 @@
 // src/services/trackService.js
-
 const BASE_URL = `${import.meta.env.MONGODB_URI}/tracks`;
 
-// Example function to fetch tracks
 export const fetchTracks = async () => {
   const response = await fetch(BASE_URL);
   return response.json();
+};
+
+export const addTrack = async (track) => {
+  const response = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(track),
+  });
+  return response.json();
+};
+
+export const updateTrack = async (trackId, track) => {
+  const response = await fetch(`${BASE_URL}/${trackId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(track),
+  });
+  return response.json();
+};
+
+export const deleteTrack = async (trackId) => {
+  await fetch(`${BASE_URL}/${trackId}`, {
+    method: 'DELETE',
+  });
 };
