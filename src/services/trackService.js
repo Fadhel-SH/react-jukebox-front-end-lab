9a98d10435dc59a8ee97b4428fb60621a38886ad
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-13 14:16:59 +0300

    Add environment variables for API URL

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,6 +1,6 @@
 // src/services/trackService.js
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+const BASE_URL = `${import.meta.env.MONGODB_URI}/tracks`;
 
 // Example function to fetch tracks
 export const fetchTracks = async () => {
